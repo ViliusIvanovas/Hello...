280f5d1647c4e0dec77747f638811bdb7fe54766
Vilius Ivanovas <ivanovasvilius@gmail.com>
2022-08-10 13:14:31 +0200

    Should be back to parity with the original project

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string hello = "Hello";
+      while (true)
+      { 
+        hello = hello + "o";
+        Console.WriteLine(hello);
+        Thread.Sleep(200);
+      }
+    }
+  }
+}
